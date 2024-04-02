69d7ea67a52ebed87e4a80c614778e1f2528d80c
ABC <abv@example.com>
2024-04-02 13:59:14 +0700

    DONE-4/2/2024

---
+++ shopbanhanglaravel/resources/views/pages/include/slider.blade.php
@@ -0,0 +1,50 @@
+<section id="slider"><!--slider-->
+        <div  class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div id="slider-carousel" class="carousel slide" data-ride="carousel">
+                        <ol class="carousel-indicators">
+                            <li data-target="#slider-carousel" data-slide-to="0" class="active"></li>
+                            <li data-target="#slider-carousel" data-slide-to="1"></li>
+                            <li data-target="#slider-carousel" data-slide-to="2"></li>
+                        </ol>
+                        <style type="text/css">
+                            img.img.img-responsive.img-slider {
+                                height:400px;
+                            }
+                           
+                        </style>
+                        <div style="width: 90%;margin:auto;" class="carousel-inner">
+                        @php 
+                            $i = 0;
+                        @endphp
+                        @foreach($slider as $key => $slide)
+                            @php 
+                                $i++;
+                            @endphp
+                            <div style="padding: 0" class="item {{$i==1 ? 'active' : '' }}">
+                                
+                                <div class="col-sm-12">
+                                    <img alt="{{$slide->slider_desc}}" src="{{asset('public/uploads/slider/'.$slide->slider_image)}}" height="200" width="100%" class="img img-responsive img-slider">
+                                   
+                                </div>
+                            </div>
+                        @endforeach  
+                          
+                            
+                        </div>
+                        
+                        <a href="#slider-carousel" class="left control-carousel hidden-xs" data-slide="prev">
+                            <i class="fa fa-angle-left"></i>
+                        </a>
+                        <a href="#slider-carousel" class="right control-carousel hidden-xs" data-slide="next">
+                            <i class="fa fa-angle-right"></i>
+                        </a>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </section><!--/slider-->
+    
+    
---
+++ shopbanhanglaravel/resources/views/pages/yeuthich/yeuthich.blade.php
@@ -0,0 +1,12 @@
+@extends('layout')
+@section('content')
+<div class="features_items"><!--features_items-->
+        <h2 class="title text-center">Sản phẩm yêu thích</h2>
+        <div id="row_wishlist" class="row">    
+
+        </div>            
+                      
+</div>
+                       
+
+@endsection
